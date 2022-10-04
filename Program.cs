788de27035668c2cbebc4311f32cfba8f2c3c926
Jiras Antonín <Jiras.An.2022@skola.ssps.cz>
2022-10-04 08:55:56 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekttak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo;
+            int cislo2;
+            int rozdil;
+
+            Console.WriteLine("zadej 1 číslo");
+            cislo = int.Parse(Console.ReadLine());
+            Console.WriteLine("zadej 2 cislo");
+            cislo2 = int.Parse(Console.ReadLine());
+
+            if (cislo == cislo2)
+            {
+                Console.WriteLine("čísla jsou stejná");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("čísla jsou jiná");
+
+                if (cislo > cislo2)
+                {
+                    rozdil = cislo - cislo2;
+                }
+                else if (cislo < cislo2)
+                {
+                    rozdil = cislo2 - cislo;
+                }
+                else
+                {
+                    rozdil = 0;
+                }
+                Console.WriteLine("Rozdíl čísel je: " + rozdil);
+            }
+            Console.ReadKey();
+        }
+    }
+}
