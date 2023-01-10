ffc60e3aafa077829806b00a42aa722a8816b154
Jiras Antonín <Jiras.An.2022@skola.ssps.cz>
2023-01-10 08:51:51 +0100

    commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hodina11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            bool active = true;
+            while (active)
+            {
+                string vstup = Console.ReadLine();
+                if (vstup == "n")
+                {
+                    break;
+                }
+                if(!int.TryParse(vstup, out int cislo))
+                {
+                    Console.WriteLine("spatne");
+                    continue;
+                }
+                list.Add(cislo);
+            }
+            Console.WriteLine("--------------------------");
+            list.Sort();
+            foreach (int op in list)
+            {
+                Console.WriteLine(op);
+            }
+            Console.ReadKey();
+        }
+    }
+}
